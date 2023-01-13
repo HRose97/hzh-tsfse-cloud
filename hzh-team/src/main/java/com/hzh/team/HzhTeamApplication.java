be9c18f8e819bc937d0d484a0e8091e653b48bfe
hou.zhonghu <hou.zhonghu@fujitsu.com>
2023-01-13 10:15:37 +0900

    1.主要提交日志管理logback-spring.xml; 2.该文件中<property name="log.path" value="E:/my-code/java/hzh-tsfse-cloud/hzh-test/test-logs" />应改为实际地址

--- hzh-team/src/main/java/com/hzh/team/HzhTeamApplication.java
+++ hzh-team/src/main/java/com/hzh/team/HzhTeamApplication.java
@@ -1,20 +1,28 @@
 package com.hzh.team;
 
 
+import com.hzh.common.utils.DateUtils;
 import com.hzh.feign.clients.EventClient;
 import com.hzh.feign.config.DefaultFeignConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.Date;
+
 
 @SpringBootApplication(scanBasePackages ={"com.hzh"})
+@Slf4j
 @EnableFeignClients(clients = EventClient.class,defaultConfiguration = DefaultFeignConfiguration.class)
 public class HzhTeamApplication {
 
     public static void main(String[] args) {
+        String date = DateUtils.getCurrent(DateUtils.ticketPattern);
+        log.info("北京时间----" +  date + "            球队中心正在启动..............");
         SpringApplication.run(HzhTeamApplication.class, args);
+        log.info("北京时间----" +  date + "            球队中心启动完成..............");
     }
 
 }
--- hzh-test/src/main/java/com/hzh/test/HzhTestApplication.java
+++ hzh-test/src/main/java/com/hzh/test/HzhTestApplication.java
@@ -1,17 +1,24 @@
 package com.hzh.test;
 
+import com.hzh.common.utils.DateUtils;
+import com.hzh.common.utils.TimeUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-@MapperScan("com.hzh.test.mysql.mapper")
+//@SpringBootApplication
+//@MapperScan("com.hzh.test.mysql.mapper")
+@SpringBootApplication(scanBasePackages ={"com.hzh"})
 public class HzhTestApplication {
 
     public static void main(String[] args) {
+        String date = DateUtils.getCurrent(DateUtils.ticketPattern);
         SpringApplication.run(HzhTestApplication.class, args);
+
     }
 
 }
